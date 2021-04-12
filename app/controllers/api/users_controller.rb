669713e9dad80763ec7d38ca5d9847c6d3e039d7
EatYourSoup7 <r.kondratowicz1@gmail.com>
2021-04-12 13:57:23 -0500

    user crud done

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -20,4 +20,25 @@ class Api::UsersController < ApplicationController
       render json: { errors: user.errors.full_messages }, status: :bad_request
     end
   end
+
+  def show
+    @user = User.find_by(id: params[:id])
+    render "show.json.jb"
+  end
+
+  def update
+    @user = User.find_by(id: params[:id])
+    @user.name = params[:name] || @user.name
+    @user.email = params[:email] || @user.email
+    @user.role = params[:role] || @user.role
+    @user.status = params[:status] || @user.status
+    @user.save
+    render "show.json.jb"
+  end
+
+  def destroy
+    @user = User.find_by(id: params[:id])
+    @user.destroy
+    render json: { message: "User deleted successfully"}
+  end
 end
