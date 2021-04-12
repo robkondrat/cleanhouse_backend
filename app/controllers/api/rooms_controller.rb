1b8b10758cdb242252d6fed130cf10089e8ac4ae
EatYourSoup7 <r.kondratowicz1@gmail.com>
2021-04-12 13:09:26 -0500

    room create and show

--- app/controllers/api/rooms_controller.rb
+++ app/controllers/api/rooms_controller.rb
@@ -3,4 +3,16 @@ class Api::RoomsController < ApplicationController
     @rooms = Room.all
     render "index.json.jb"
   end
+
+  def create
+    @room = Room.new(
+      number: params[:number],
+      floor: params[:floor],
+      room_type: params[:room_type],
+      status: params[:status]
+    )
+    @room.save
+    render "show.json.jb"
+  end
+
 end
