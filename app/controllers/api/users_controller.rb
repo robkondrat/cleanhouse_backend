61a1764455e2da592e827df20265d68e47a1ac9d
EatYourSoup7 <r.kondratowicz1@gmail.com>
2021-04-12 13:44:42 -0500

    user create

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -3,4 +3,21 @@ class Api::UsersController < ApplicationController
     @users = User.all
     render "index.json.jb"
   end
+
+  def create
+    @user = User.new(
+      name: params[:name],
+      email: params[:email],
+      password: params[:password],
+      password_confirmation: params[:password_confirmation],
+      role: params[:role],
+      status: params[:status]
+    )
+
+    if @user.save
+      render json: { message: "User created succesfully" }, status: :created
+    else
+      render json: { errors: user.errors.full_messages }, status: :bad_request
+    end
+  end
 end
