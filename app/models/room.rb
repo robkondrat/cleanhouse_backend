c817ed07fbfa37a6b5cdd9870dc4dc0387d248ff
EatYourSoup7 <r.kondratowicz1@gmail.com>
2021-04-20 18:27:20 -0500

    fixed CORS issues

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,8 +1,8 @@
 class Room < ApplicationRecord
   belongs_to :user
-  
+
   validates :number, presence: true, uniqueness: true, numericality: { greater_than: 0 }
   validates :floor, presence: true, numericality: { greater_than: 0 }
   validates :status, presence: true
-  validates :type, presence: true
+  validates :room_type, presence: true
 end
